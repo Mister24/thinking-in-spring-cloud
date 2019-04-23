312053e4aa659c111486f4d296544a7beec477bf
Mister24 <motivated24@hotmail.com>
2019-04-23 15:12:15 +0800

    update gitignore

---
+++ src/main/java/com/fayuan/thinkinginspringcloud/ThinkingInSpringCloudApplication.java
@@ -0,0 +1,13 @@
+package com.fayuan.thinkinginspringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkingInSpringCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThinkingInSpringCloudApplication.class, args);
+    }
+
+}
