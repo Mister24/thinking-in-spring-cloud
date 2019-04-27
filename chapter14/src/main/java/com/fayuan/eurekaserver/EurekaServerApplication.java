b6759eefc7fe9918d25adde5e1b89538532de5af
Mister24 <motivated24@hotmail.com>
2019-04-27 14:23:55 +0800

    eureka server初始化

--- chapter14/src/main/java/com/fayuan/eurekaserver/EurekaServerApplication.java
+++ chapter14/src/main/java/com/fayuan/eurekaserver/EurekaServerApplication.java
@@ -1,13 +1,13 @@
-package com.fayuan.chapter14;
+package com.fayuan.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Chapter14Application {
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Chapter14Application.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
 }
