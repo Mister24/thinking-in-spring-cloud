740fddf8ab6b5a3c069842c400a9589dda2f6a43
Mister24 <motivated24@hotmail.com>
2019-06-06 22:15:06 +0800

    add eureka bundle

---
+++ eureka-client/src/main/java/com/fayuan/eurekaclient/controller/HiController.java
@@ -0,0 +1,26 @@
+/**
+ * @Company Mister24.com Inc.
+ * @Copyright Copyright (c) 2016-2019 All Rights Reserved.
+ */
+package com.fayuan.eurekaclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * controller
+ *
+ * @author mr.24
+ * @version Id: HiController, v 1.0 2019-06-06 15:59 Exp $$
+ */
+public class HiController {
+
+    @Value("server.port")
+    public String port;
+
+    @GetMapping("/hi")
+    public String home(@RequestParam String name) {
+        return "hi " + name + ",i am from port: " + port;
+    }
+}
--- ribbon/src/main/java/com/fayuan/ribbon/service/RibbonService.java
+++ ribbon/src/main/java/com/fayuan/ribbon/service/RibbonService.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 /**
- *
  * @author mr.24
  * @version Id: RibbonService, v 1.0 2019-06-06 00:47 Exp $$
  */
@@ -17,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
 public class RibbonService {
     @Autowired
     RestTemplate restTemplate;
+
     public String hi(String name) {
         return restTemplate.getForObject("http://eureka-client/hi?name=" + name, String.class);
     }
