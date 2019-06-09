9c66fb0b66a99843ff14d23ebdcc468521d88adb
Mister24 <motivated24@hotmail.com>
2019-06-09 23:03:05 +0800

    add eureka configuration

--- eureka-feign-client/src/main/java/com/fayuan/eurekafeignclient/EurekaFeignClientApplication.java
+++ eureka-feign-client/src/main/java/com/fayuan/eurekafeignclient/EurekaFeignClientApplication.java
@@ -2,8 +2,10 @@ package com.fayuan.eurekafeignclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EurekaFeignClientApplication {
 
     public static void main(String[] args) {
